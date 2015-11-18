aa4d45a07f8ce0b6a613c9ce0ff766092d57965e
Kevin Canévet <kevin@streamroot.io>
2015-11-18 18:03:52 +0100

    rename “TrackerManager” to signalingChannel and use a factory instead of a class constructor

--- app/client/signalingChannelFactory.js
+++ app/client/signalingChannelFactory.js
@@ -1,6 +1,7 @@
-function TrackerManager(){
+function SignalingChannel(){
 
     var _ws;
+    var self = this;
 
     function connectToTracker(url){
         _ws = new WebSocket(url);
@@ -27,13 +28,13 @@ function TrackerManager(){
         var objMessage = JSON.parse(evt.data);
         switch (objMessage.type) {
             case "ICECandidate":
-                _onICECandidate(objMessage.ICECandidate, objMessage.source);
+                self.onICECandidate(objMessage.ICECandidate, objMessage.source);
                 break;
             case "offer":
-                _onOffer(objMessage.offer, objMessage.source);
+                self.onOffer(objMessage.offer, objMessage.source);
                 break;
             case "answer":
-                _onAnswer(objMessage.answer, objMessage.source);
+                self.onAnswer(objMessage.answer, objMessage.source);
                 break;
             default:
                 throw new Error("invalid message type");
@@ -46,18 +47,6 @@ function TrackerManager(){
         message[type] = data;
         message.destination = destination;
         _ws.send(JSON.stringify(message));
-    }
-    
-    function _onOffer(offer, source){
-        console.log("offer from peer:", source, ':', offer);
-    }
-
-    function _onAnswer(answer, source){
-        console.log("answer from peer:", source, ':', answer);
-    }
-
-    function _onICECandidate(ICECandidate, source){
-        console.log("ICECandidate from peer:", source, ':', ICECandidate);
     }
 
     function sendICECandidate(ICECandidate, destination){
@@ -77,6 +66,25 @@ function TrackerManager(){
     this.sendICECandidate = sendICECandidate;
     this.sendOffer = sendOffer;
     this.sendAnswer = sendAnswer;
+
+    //default handler, should be overriden 
+    this.onOffer = function(offer, source){
+        console.log("offer from peer:", source, ':', offer);
+    };
+
+    //default handler, should be overriden 
+    this.onAnswer = function(answer, source){
+        console.log("answer from peer:", source, ':', answer);
+    };
+
+    //default handler, should be overriden 
+    this.onICECandidate = function(ICECandidate, source){
+        console.log("ICECandidate from peer:", source, ':', ICECandidate);
+    };
 }
 
-window.TrackerManager = TrackerManager;
+window.createSignalingChannel = function(url){
+    var signalingChannel = new SignalingChannel();
+    signalingChannel.connectToTracker(url);
+    return signalingChannel;
+};
